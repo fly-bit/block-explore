618cbcccf190da9fa7b9ba04a16a78e2aab3b19f
Stephen Zhang <stephenatcom@gmail.com>
2018-01-18 17:36:21 +0800

    add pagination function

--- src/main/java/com/flybit/blockchain/controller/ShowBlocksController.java
+++ src/main/java/com/flybit/blockchain/controller/ShowBlocksController.java
@@ -1,16 +1,21 @@
 package com.flybit.blockchain.controller;
 
+import com.flybit.blockchain.domain.Pager;
 import com.flybit.blockchain.services.ShowBlocksServices;
 import com.flybit.blockchain.vo.BlocksVO;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ShowBlocksController {
@@ -56,4 +61,33 @@ public class ShowBlocksController {
         model.addAttribute("blocksVOList", blocksVOList);
         return "blocks";
     }
+
+    private static final int BUTTONS_TO_SHOW = 5;
+    private static final int INITIAL_PAGE = 0;
+    private static final int INITIAL_PAGE_SIZE = 5;
+    private static final int[] PAGE_SIZES = {5, 10, 20};
+
+    @RequestMapping("/paging")
+    public ModelAndView blocksInPaging(@RequestParam("pageSize") Optional<Integer> pageSize,
+                                       @RequestParam("page") Optional<Integer> page) {
+        ModelAndView modelAndView = new ModelAndView("pagingBlocks");
+
+        // Evaluate page size. If requested parameter is null, return initial
+        // page size
+        int evalPageSize = pageSize.orElse(INITIAL_PAGE_SIZE);
+        // Evaluate page. If requested parameter is null or less than 0 (to
+        // prevent exception), return initial size. Otherwise, return value of
+        // param. decreased by 1.
+        int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
+
+        Page<BlocksVO> blocksVOS = showBlocksServices.findAllPageable(new PageRequest(evalPage, evalPageSize));
+        Pager pager = new Pager(blocksVOS.getTotalPages(), blocksVOS.getNumber(), BUTTONS_TO_SHOW);
+
+        modelAndView.addObject("blocksVOS", blocksVOS);
+        modelAndView.addObject("selectedPageSize", evalPageSize);
+        modelAndView.addObject("pageSizes", PAGE_SIZES);
+        modelAndView.addObject("pager", pager);
+        return modelAndView;
+    }
+
 }
--- src/main/java/com/flybit/blockchain/services/ShowBlocksServices.java
+++ src/main/java/com/flybit/blockchain/services/ShowBlocksServices.java
@@ -2,6 +2,9 @@ package com.flybit.blockchain.services;
 
 import com.flybit.blockchain.vo.BlocksVO;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -20,6 +23,33 @@ public class ShowBlocksServices {
     public List<BlocksVO> fetchAllBlocks() {
         List<BlocksVO> blocksVOList = new ArrayList<>();
         blocksVOList = getMockAllData();
+        return blocksVOList;
+    }
+
+    public Page<BlocksVO> findAllPageable(Pageable pageable) {
+        List<BlocksVO> blocksVOList = new ArrayList<>();
+        blocksVOList = getMockAllData(pageable);
+        Page<BlocksVO> result = new PageImpl<>(blocksVOList, pageable, pageable.getPageSize() * 4 + 1);
+        return result;
+    }
+
+    private List<BlocksVO> getMockAllData(Pageable pageable) {
+        List<BlocksVO> blocksVOList = new ArrayList<>();
+        if (pageable.getPageNumber() == 4) {
+            BlocksVO blocksVO1 = new BlocksVO();
+            blocksVO1.setHeight("5x5x5x");
+            blocksVO1.setHarvester("nbfpzg-5x5x5x-5ijizy-4knhaz-dntttq-yso7ih-idfb");
+            blocksVOList.add(blocksVO1);
+            return blocksVOList;
+        }
+        for (int i = 1; i <= pageable.getPageSize(); i++) {
+            int height = i * (pageable.getPageNumber() + 1);
+            BlocksVO blocksVO1 = new BlocksVO();
+            String str = "" + height + height + height + height;
+            blocksVO1.setHeight(str);
+            blocksVO1.setHarvester("nbfpzg-" + str + "-5ijizy-4knhaz-dntttq-yso7ih-idfb");
+            blocksVOList.add(blocksVO1);
+        }
         return blocksVOList;
     }
 
